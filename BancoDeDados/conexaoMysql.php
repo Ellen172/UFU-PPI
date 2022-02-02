f3d6098fcf68b1a2ca4dae9b38043088ad303fa0
EllenCas172 <ellencas172@gmail.com>
2022-02-02 19:14:19 -0300

    aula banco de dados

--- BancoDeDados/conexaoMysql.php
+++ BancoDeDados/conexaoMysql.php
@@ -2,17 +2,17 @@
 
 function mysqlConnect()
 {
-  $db_host = "fdb7.awardspace.net";
-  $db_username = "seu_usuario_no_mysql";
-  $db_password = "senha_do_usuario";
-  $db_name = "nome_do_banco_de_dados";
+  $db_host = "fdb7.awardspace.net"; // endereço do servido do MySQL
+  $db_username = "seu_usuario_no_mysql"; // usuario para acessar banco de dados
+  $db_password = "senha_do_usuario"; // senha para acessar banco de dados
+  $db_name = "nome_do_banco_de_dados"; // nome do banco de dados ao qual se pretende acessar
 
   // dsn é apenas um acrônimo de database source name
   $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
 
   $options = [
-    PDO::ATTR_EMULATE_PREPARES => false, // desativa a execução emulada de prepared statements
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // ativa o modo de erros para lançar exceções    
+    PDO::ATTR_EMULATE_PREPARES => false, // desativa a execução emulada de prepared statements (executam de forma nativa)
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // erros tratado atraves do lançamento de exceções    
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // altera o modo padrão do método fetch para FETCH_ASSOC
   ];
 
@@ -26,4 +26,20 @@ function mysqlConnect()
   }
 }
 
+/*
+Executar MySql
+
+  $pdo->prepare("Codigo SQL");
+  $pdo->execute(...) ;
+  // utilizado quando o código inclui dados fornecidos pelos usuario
+  
+  $pdo->exec("Código SQL"); 
+  // utilizado quando a operação não devolve nenhum resultado, não há processamento do resultado
+
+  $pdo->query("Código SQL");
+  // há resultado para processamento
+*/
+
 ?>
+
+
