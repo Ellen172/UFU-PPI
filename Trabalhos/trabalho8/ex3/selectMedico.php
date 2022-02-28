74a93c18e2542524bfcd82dd4839645a855efde5
EllenCas172 <ellencas172@gmail.com>
2022-02-28 20:22:07 -0300

    solucao ex3

---
+++ Trabalhos/trabalho8/ex3/selectMedico.php
@@ -0,0 +1,38 @@
+<?php
+require "/home/www/ppi-12011bsi208-trabalho1.atwebpages.com/conexaoMysql.php";
+$pdo = mysqlConnect();
+
+class Medico {
+    public $nome;
+    public $telefone; 
+
+    function __construct($nome, $telefone)
+    {
+        $this->nome = $nome;
+        $this->telefone = $telefone;
+    }
+}
+
+try {
+    $especialidade = $_GET["especialidade"] ?? "";
+
+    $sql = <<<SQL
+        SELECT nome, telefone
+        FROM medico
+        WHERE especialidade = ?
+        SQL;
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$especialidade]);
+
+    $medicos = [];
+    while ($row = $stmt->fetch()) {
+        $medico = new Medico(htmlspecialchars($row['nome']), htmlspecialchars($row['telefone']));
+        array_push($medicos, $medico);
+    }
+    
+    echo json_encode($medicos);
+}
+catch (Exception $e) {
+  exit('Falha inesperada: ' . $e->getMessage());
+}
