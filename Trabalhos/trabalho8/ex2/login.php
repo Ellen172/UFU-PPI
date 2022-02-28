0c1aa30ea687031e62417372935470e9d91c9286
EllenCas172 <ellencas172@gmail.com>
2022-02-28 20:04:28 -0300

    solução ex2

---
+++ Trabalhos/trabalho8/ex2/login.php
@@ -0,0 +1,43 @@
+<?php
+
+require "/home/www/ppi-12011bsi208-trabalho1.atwebpages.com/conexaoMysql.php";
+$pdo = mysqlConnect();
+
+class RequestResponse
+{
+  public $success;
+  public $destination;
+  function __construct($success, $destination)
+  {
+    $this->success = $success;
+    $this->destination = $destination;
+  }
+}
+
+try {
+  $sql = <<<SQL
+    SELECT hash_senha
+    FROM usuario
+    WHERE email = ?
+    SQL;
+
+  $email = $_POST["email"] ?? "";
+  $senha = $_POST["senha"] ?? "";
+  
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([$email]);
+  $row = $stmt->fetch();
+
+  if (!$row) $success=false; // nenhum resultado (email não encontrado)
+  
+  $success=password_verify($senha, $row['hash_senha']);
+  
+  $response = new RequestResponse($success, "home.html");  
+
+  echo json_encode($response);
+} 
+catch (Exception $e) {
+  exit('Falha inesperada: ' . $e->getMessage());
+}
+
+?>
